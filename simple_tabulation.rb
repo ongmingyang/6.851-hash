fc761fea349fd0464e25a43e8a9f3dd4a8962f62
Ong Ming Yang <mingy@mit.edu>
2014-05-11 04:08:04 -0400

    Working simple tabulation hash

--- simple_tabulation.rb
+++ simple_tabulation.rb
@@ -1,26 +1,28 @@
-T1 = Array.new
-T2 = Array.new
-T3 = Array.new
-T4 = Array.new
-T5 = Array.new
-T6 = Array.new
-T7 = Array.new
-T8 = Array.new
+T = Array.new
 
 # I need 8 tables, each of size 8 bits. Each table entry points to a 64 bit word.
 open('RandomNumbers', 'rb') do |file|
-  for array in [T1, T2, T3, T4, T5, T6, T7, T8]
-    for i in 1..256
-      array << file.read(8).unpack('Q')[0]
+  for i in 0..7
+    T[i] = Array.new
+    for j in 0..254
+      T[i] << file.read(8).unpack('Q')[0]
     end
+    puts T[i].size
   end
 end
 
-print T1
-print T2
-print T3
-print T4
-print T5
-print T6
-print T7
-print T8
+# accepts a 64 bit integer x and returns its simple tabulation hash
+def mash(x)
+  c = Array.new
+  c[0] = x & 18374686479671623680 << -56 # 11111111(0)^56
+  c[1] = x & 71776119061217280 << -48 # 11111111(0)^48
+  c[2] = x & 280375465082880 << -40 # 11111111(0)^40
+  c[3] = x & 1095216660480 << -32 # 11111111(0)^32
+  c[4] = x & 4278190080 << -24 # 11111111(0)^24
+  c[5] = x & 16711680 << -16 # 11111111(0)^16
+  c[6] = x & 589056 << -8 # 11111111(0)^8
+  c[7] = x & 255 #11111111
+
+  h = T[0][c[0]]^T[1][c[1]]^T[2][c[2]]^T[3][c[3]]^T[4][c[4]]^T[5][c[5]]^T[6][c[6]]^T[7][c[7]]
+end
+
